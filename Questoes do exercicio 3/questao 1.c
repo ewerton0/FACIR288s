a8b2d64c537958fed486a494231f638e9ee4b88b
ewerton0 <54162965+ewerton0@users.noreply.github.com>
2019-09-01 14:42:10 -0300

    Add files via upload

---
+++ Questoes do exercicio 3/questao 1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float sf, vv, utt, sts, stn;
+    printf("Digite o seu salario fixo:\n");
+    scanf("%f", &sf);
+    printf("Digie o valor das vendas efetuadas pelo vendedor da empresa:\n");
+    scanf("%f", &vv);
+    utt = vv - 1500;
+    if(vv >= 1500){
+        sts = sf + (vv * 0.03) + (utt * 0.05);
+        printf("O seu salario total é: %.2f\n", sts);
+    }else{
+        stn = sf + (vv * 0.03);
+        printf("O seu salario total é: %.2f\n", stn);
+        }
+
+
+
+
+    return 0;
+}
