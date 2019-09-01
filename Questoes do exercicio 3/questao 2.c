d8bc31904514be4511e62fecff314326511e924f
ewerton0 <54162965+ewerton0@users.noreply.github.com>
2019-09-01 15:22:37 -0300

    Update questao 2.c

--- Questoes do exercicio 3/questao 2.c
+++ Questoes do exercicio 3/questao 2.c
@@ -3,15 +3,18 @@
 
 int main()
 {
+    char nc[20];
     int s, d, c, sa;
+    printf("digite o numero da conta:\n");
+    scanf("%s", &nc);
     printf("digite seu saldo:\n");
     scanf("%i", &s);
     printf("digite seu credito:\n");
-    scanff("%i", &c);
+    scanf("%i", &c);
     printf("digite seu debito:\n");
     scanf("%i", &d);
-    sa = s + d + c;
-    if sa >= 0{
+    sa = s - d + c;
+    if (sa >= 0){
     printf("Apresentar saldo positivo");
    }else{
         printf("Apresentar saldo negativo");}
